0c27e1310f5d759154cffddfa42b6360596d20a2
Haruji Shigemori <test0302sh@gmail.com>
2021-02-11 15:51:30 +0900

    Initial Commit

---
+++ DailyYamazaki/Controller/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  DailyYamazaki
+//
+//  Created by 重盛晴二 on 2021/02/11.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DailyYamazaki/Controller/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarController.swift
+//  DailyYamazaki
+//
+//  Created by 重盛晴二 on 2021/02/11.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UITabBar.appearance().tintColor = UIColor.orange
+
+    }
+}
